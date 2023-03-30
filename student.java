cd63336e36cb7f101386f4b1f431330f9828c51e
laxmidhanu <127470276+laxmidhanu@users.noreply.github.com>
2023-03-30 20:10:09 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,13 @@
+package package_eg;
+
+public class student {
+	int age=27;
+	String name= "laxmi"'
+	String address="solapur";
+	public static void main (String args []) {
+	student obj= new student();
+	//System.out.println(obj.age+""+obj.name""+obj.address);
+	System.out.println( )
+	}
+
+}
